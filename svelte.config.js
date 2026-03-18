da8ab343a39c4d7020dc72a57b8d2d335b51e8e5
WillItRun <mehtayashveer09@gmail.com>
2026-03-18 18:43:48 +0530

    fix: add base path for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		})
-	},
-	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) =>
-			filename.includes('node_modules') ? undefined : { runes: true }
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: '/willitrun'
+    }
+  },
+  vitePlugin: {
+    dynamicCompileOptions: ({ filename }) =>
+      filename.includes('node_modules') ? undefined : { runes: true }
+  }
 };
 
 export default config;
